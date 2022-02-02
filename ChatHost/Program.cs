7cae4d23b2b0ee9cedc7605b1f719936e4f02f3f
includingivansmirnov@gmail.com <includingivansmirnov@gmail.com>
2022-02-02 17:04:39 +0500

    add host proj

---
+++ ChatHost/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatHost
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var host = new ServiceHost(typeof(wcf_chat.ServiceChat)))
+            {
+                host.Open();
+                Console.WriteLine("Host has been started");
+                Console.ReadLine();
+            }
+        }
+    }
+}
